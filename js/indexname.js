c3837c7e507b33e304057df85dcec07d5bb2abff
lindmark421@gmail.com <lindmark421@gmail.com>
2019-08-14 16:32:21 -0400

    added name and js script

---
+++ js/indexname.js
@@ -0,0 +1,12 @@
+
+function fullname() {
+
+        
+    
+    var first = document.getElementById("firstname").value;
+    
+    var last = document.getElementById("lastname").value;
+    
+     console.log(first, last)
+
+}
